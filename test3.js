86a625c238b84712654b7b44f350401d81c0f8e8
cottignolima <43776185+cottignolima@users.noreply.github.com>
2019-05-16 11:34:54 +0200

    Add files via upload

---
+++ test3.js
@@ -0,0 +1,33 @@
+class NitroBlockA2 {
+    getInfo() {
+        return {
+            "id": "NitroBlockA2",
+            "name": "NitroBlockA2",
+            "blocks": [{
+                       "opcode": "substringy",
+                       "blockType": "reporter",
+                       "text": "letters [num1] through [num2] of [string]",
+                       "arguments": {
+                       "num1": {
+                       "type": "number",
+                       "defaultValue": "2"
+                       },
+                       "num2": {
+                       "type": "number",
+                       "defaultValue": "5"
+                       },
+                       "string": {
+                       "type": "string",
+                       "defaultValue": "hello world"
+                       }
+                       }
+                       },
+                       }],
+            "menus": { //we will get back to this in a later tutorial
+            }
+        };
+        substringy({num1, num2, string}) {
+            return string.substring(num1 - 1, num2);
+        };
+    }
+    Scratch.extensions.register(new NitroBlockA2());
