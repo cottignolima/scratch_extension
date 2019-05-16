37e7626aebeff4f0deba36d011b90aea84efbc2c
cottignolima <43776185+cottignolima@users.noreply.github.com>
2019-05-16 11:31:59 +0200

    Update test.js

--- test.js
+++ test.js
@@ -26,6 +26,7 @@ class NitroBlockAAA {
         "menus": { //we will get back to this in a later tutorial
         }
     };
+    }
     substringy({num1, num2, string}) {
         return string.substring(num1 - 1, num2);
     };
