e1d01307f7ad256d0c3ad7ce21ac0aeb8be4de37
cottignolima <43776185+cottignolima@users.noreply.github.com>
2019-05-18 09:35:14 +0200

    Update Network.js

--- Network.js
+++ Network.js
@@ -1,61 +1,95 @@
 class Network {
-    getInfo() {
-        return {
-            "id": "Network",
-            "name": "Network",
-            "blocks": [{
-                    "opcode": "substringy",
-                    "blockType": "reporter",
-                    "text": "letters [num1] through [num2] of [string]",
-                    "arguments": {
-                        "num1": {
-                            "type": "number",
-                            "defaultValue": "2"
-                        },
-                        "num2": {
-                            "type": "number",
-                            "defaultValue": "5"
-                        },
-                        "string": {
-                            "type": "string",
-                            "defaultValue": "hello world"
-                        }
-                    }
-            },
-            {
-                    "opcode": "sendcoord",
-                    "blockType": "reporter",
-                    "text": "id [id] x: [x] y: [y]",
-                    "arguments": {
-                        "x": {
-                            "type": "number",
-                            "defaultValue": "0"
-                        },
-                        "y": {
-                            "type": "number",
-                            "defaultValue": "0"
-                        },
-                        "id": {
-                            "type": "string",
-                            "defaultValue": "hello world"
-                        }
-                    }
-            },
-            
-            ],
-        "menus": { //we will get back to this in a later tutorial
-        }
-    };
-    }
-    substringy({num1, num2, string}) {
-        return string.substring(num1 - 1, num2);
-    };
-    sendcoord({id, x, y}) {
-        //const sprites = vm.runtime.targets.filter(target => !target.isStage);
-        //const data = sprites.map(sprite => ({x: sprite.x, y: sprite.y}));
-	//    console.log(data);
-        //s = JSON.stringify(data);
-	return JSON.stringify({id, x, y});
-    };
+	
+	var ws;
+	
+	getInfo() {
+		return {
+			"id": "Network",
+			"name": "Network",
+			"blocks": [
+				{
+					"opcode": "webAvailable",
+					"blockType": "Boolean",
+					"text": "available",
+					"arguments": {
+					}
+				},
+				{
+					"opcode": "open",
+					"blockType": "reporter",
+					"text": "address",
+					"arguments": {
+						"addr": {
+							"type": "string",
+							"defaultValue": "localhost:9999"
+						}
+					}
+				},
+				{
+					"opcode": "sendcoord",
+					"blockType": "reporter",
+					"text": "id [id] x: [x] y: [y]",
+					"arguments": {
+						"x": {
+							"type": "number",
+							"defaultValue": "0"
+						},
+						"y": {
+							"type": "number",
+							"defaultValue": "0"
+						},
+						"id": {
+							"type": "string",
+							"defaultValue": "hello world"
+						}
+					}
+				},
+
+				],
+				"menus": { //we will get back to this in a later tutorial
+				}
+		};
+	}	
+
+	webAvailable(){
+		return "WebSocket" in window;
+	}
+
+	open(addr){
+		// Let us open a web socket
+		ws = new WebSocket("ws://"+addr);
+	}
+
+	onopen(){
+		ws.onopen = function() {
+
+			// Web Socket is connected, send data using send()
+			ws.send("Message to send");
+			alert("Message is sent...");
+		};
+	}
+
+	onmessage(){
+		ws.onmessage = function (evt) { 
+			var received_msg = evt.data;
+			alert("Message is received...");
+		};
+	}
+
+	onclose(){
+		ws.onclose = function() { 
+
+			// websocket is closed.
+			alert("Connection is closed..."); 
+		};
+	}
+
+	sendcoord({id, x, y}) {
+		//const sprites = vm.runtime.targets.filter(target => !target.isStage);
+		//const data = sprites.map(sprite => ({x: sprite.x, y: sprite.y}));
+		//    console.log(data);
+		//s = JSON.stringify(data);
+		return JSON.stringify({id, x, y});
+	};
 }
 Scratch.extensions.register(new Network());
