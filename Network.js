35dce9789814d99e4aa9b01d9fb9597574d87d91
cottignolima <43776185+cottignolima@users.noreply.github.com>
2019-05-22 12:31:41 +0200

    Update Network.js

--- Network.js
+++ Network.js
@@ -1,6 +1,13 @@
 class Network {
 
-
+constructor (runtime) {
+        /**
+         * Store this for later communication with the Scratch VM runtime.
+         * If this extension is running in a sandbox then `runtime` is an async proxy object.
+         * @type {Runtime}
+         */
+        this.runtime = runtime;
+    }
 
 	getInfo() {
 		return {
